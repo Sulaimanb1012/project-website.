a08da05a7383eb88894392a2e059f593f226281c
Sulaimanb1012 <9024289@student.tcrmbo.nl>
2025-10-18 16:02:02 +0200

    project

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Services.css";
 import Services_Data from "../../assets/services_data";
